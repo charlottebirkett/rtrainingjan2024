ad3fef4b1cf7d7942b64e6d80ba87f2129149c91
Charlotte Birkett <charlotte.birkett@nhm.ac.uk>
2024-01-25 12:56:43 +0000

    Found a fun trick using #*

--- rtrainingjan2024script.R
+++ rtrainingjan2024script.R
@@ -3,6 +3,16 @@ library(ggplot2)
 print(23700187493*524932019740973)
 print(34875293475874/294785847)
 
-#Hi, how is your cat?
-
-#Fine thanks, she really likes tuna
+#*Oh, I just found out adding a * after a # lets you press enter and it carries it to the next line so you can type
+#*with fewer interruptions
+#*ooh
+#*
+#*This is much easier
+#*
+#*
+#*Then you just delete it when you need to go back to usual coding
+#*
+#*
+#*
+#*
+#*
